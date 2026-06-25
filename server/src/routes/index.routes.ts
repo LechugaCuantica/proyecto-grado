746725314a9fbb826208b2659b7edf32451c32fb
Camilo <osoriojuancamilo316@gmail.com>
2026-06-25 18:18:56 -0500

    fix

--- server/src/routes/index.routes.ts
+++ server/src/routes/index.routes.ts
@@ -7,6 +7,6 @@ router.get("/", validateLogin);
 router.post("/login", login);
 router.post("/register", register);
 
-router.use("/dashboard", usersRoutes);;
+router.use("/dashboard", usersRoutes);
 
 export default router;
